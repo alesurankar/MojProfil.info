f09eeee0dc8d9bfd63a59227db24197f9a83b124
ales2026 <ale.urankar@gmail.com>
2025-08-02 16:13:42 +0200

    images appearing

--- MemoryGame/app.js
+++ MemoryGame/app.js
@@ -52,14 +52,23 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+const cardsChosen = []
 
 function createBoard() {
     for(let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
     }
 }
 
 createBoard()
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardArray[cardId].name
+    cardsChosen.push(cardArray[cardId.name])
+    this.setAttribute('src', cardArray[cardId].img)
+}
