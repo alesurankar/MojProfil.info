f6f686f87baa315e7e87bcd258cc70ec248e2356
ales2026 <ale.urankar@gmail.com>
2025-08-06 07:29:23 +0200

    Portfolio Website with dark Mode_9

--- Profil2/script.js
+++ Profil2/script.js
@@ -5,6 +5,19 @@ cloneContainer.id = 'dark-container';
 document.body.appendChild(cloneContainer);
 cloneContainer.classList.remove('active');
 
+const toggleIcons = document.querySelectorAll('.toggle-icon');
+const icons = document.querySelectorAll('.toggle-icon i');
+const darkContainer = document.querySelector('#dark-container');
 const darkContainerImg = document.querySelector('#dark-container .home-img img');
 
 darkContainerImg.src = 'imgDark.png';
+
+toggleIcons.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+
+        icons.forEach(icon => {
+            icon.classList.toggle('bx-sun');
+        })
+
+    })
+})
