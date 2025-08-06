a6472ac2c043cabf8832fabcc0d642cfb0193f40
ales2026 <ale.urankar@gmail.com>
2025-08-06 07:17:13 +0200

    Portfolio Website with dark Mode_7

--- Profil2/script.js
+++ Profil2/script.js
@@ -2,5 +2,9 @@ const container = document.querySelector('.container');
 
 const cloneContainer = container.cloneNode(true);
 cloneContainer.id = 'dark-container';
+document.body.appendChild(cloneContainer);
+cloneContainer.classList.remove('active');
 
-console.log(cloneContainer);
+const darkContainerImg = document.querySelector('#dark-container .home-img img');
+
+darkContainerImg.src = 'imgDark.png';
